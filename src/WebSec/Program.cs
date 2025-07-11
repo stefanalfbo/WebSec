a04e11d6c6d9137a3c7bb8ac32a079b47e90e6ce
Stefan Alfbo <stefan@alfbo.se>
2025-07-11 20:51:36 +0000

    Add spectre as a dependency to the WebSec project

--- src/WebSec/Program.cs
+++ src/WebSec/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        AnsiConsole.Markup("[underline red]Hello[/] World!");
+    }
+}
